e2b42cb70416e282efc3177a80ca5e32d10262b5
Ben <Ben@test.com>
2017-10-19 13:57:57 -0500

    fix bug

--- models.py
+++ models.py
@@ -149,7 +149,7 @@ class Client(db.Model):
     __tablename__ = 'Client'
     id = db.Column('client_id',db.Integer , primary_key=True)
     admin_id = db.Column(db.Integer, db.ForeignKey('Admin.user_id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), nullable=True)
     application_id = db.Column(db.Integer)
     vm_id = db.Column(db.Integer)
     name = db.Column(db.String(128))
